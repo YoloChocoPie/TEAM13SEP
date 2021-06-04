d26ea54a6ed8e9c44ec24b6bff0e3c536676104e
YoloChocoPie <chocopieyolo@gmail.com>
2021-06-05 01:32:59 +0700

    Add ChuDe Controller + Add View

---
+++ TEAM13SEP/Areas/Admin/Controllers/ChuDeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TEAM13SEP.Models;
+
+namespace TEAM13SEP.Areas.Admin.Controllers
+{
+    public class ChuDeController : Controller
+    {
+        SEPEntities model = new SEPEntities();
+        // GET: Admin/ChuDe
+        public ActionResult Index()
+        {
+            var chude = model.CHUDEs.OrderByDescending(x => x.ID).ToList();
+            return View(chude);
+        }
+    }
+}

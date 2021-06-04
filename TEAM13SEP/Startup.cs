e73db31629d4b3d3de37cf11eba39b2ed534dd4e
YoloChocoPie <chocopieyolo@gmail.com>
2021-06-04 23:19:54 +0700

    Add project files.

---
+++ TEAM13SEP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TEAM13SEP.Startup))]
+namespace TEAM13SEP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

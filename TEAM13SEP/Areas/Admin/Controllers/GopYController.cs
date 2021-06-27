6f73dc3eb5b91f7f1b0804974c17a97e659b1642
TrungNguyen2203 <tntn2203@gmail.com>
2021-06-27 15:06:10 +0700

    Cập nhập phiên bản mới nhất của dự án

--- TEAM13SEP/Areas/Admin/Controllers/GopYController.cs
+++ TEAM13SEP/Areas/Admin/Controllers/GopYController.cs
@@ -4,12 +4,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-//using TEAM13SEP.Areas.Admin.Middleware;
+using TEAM13SEP.Areas.Admin.Middleware;
 using TEAM13SEP.Models;
 
 namespace TEAM13SEP.Areas.Admin.Controllers
 {
-    //[LoginVerification]
+    [LoginVerification]
     public class GopYController : Controller
     {
         // GET: Admin/GopY
@@ -75,7 +75,6 @@ namespace TEAM13SEP.Areas.Admin.Controllers
             {
                 return HttpNotFound();
             }
-            // lay thuoc tinh tu bang chu de, admin, trang thai
             ViewBag.chude_id = model.CHUDEs.OrderByDescending(x => x.ID).ToList();
             ViewBag.admin_id = model.ADMINs.OrderByDescending(x => x.ID).ToList();
             ViewBag.status_id = model.TRANGTHAIs.OrderByDescending(x => x.ID).ToList();
--- TEAM13SEP/Areas/User/Middleware/LoginVerification.cs
+++ TEAM13SEP/Areas/User/Middleware/LoginVerification.cs
@@ -1,4 +1,4 @@
-﻿
+
 using System.Web.Mvc;
 
 namespace TEAM13SEP.Areas.User.Middleware

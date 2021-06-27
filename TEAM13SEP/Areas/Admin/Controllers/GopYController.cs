d4b39aa5194bceaf757bfaa0ff75e9394a4ded6f
187PM13935 - Dang Tuan Kha - K24T-PM1 <khadang1535@gmail.com>
2021-06-27 15:10:00 +0700

    Cập nhập phiên bản mới nhất của dự án

--- TEAM13SEP/Areas/Admin/Controllers/GopYController.cs
+++ TEAM13SEP/Areas/Admin/Controllers/GopYController.cs
@@ -1,16 +1,14 @@
 ﻿
 using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using TEAM13SEP.Areas.User.Middleware;
+using TEAM13SEP.Areas.Admin.Middleware;
 using TEAM13SEP.Models;
 
-namespace TEAM13SEP.Areas.User.Controllers
+namespace TEAM13SEP.Areas.Admin.Controllers
 {
-
     [LoginVerification]
     public class GopYController : Controller
     {
@@ -21,16 +19,13 @@ namespace TEAM13SEP.Areas.User.Controllers
             var gopy = model.GOPies.OrderByDescending(x => x.ID).ToList();
             return View(gopy);
         }
-        public ActionResult Index2()
-        {
-            var gopy = model.GOPies.OrderByDescending(x => x.ID).ToList();
-            return View(gopy);
-        }
         [HttpGet]
         public ActionResult Create()
         {
             ViewBag.chude_id = model.CHUDEs.OrderByDescending(x => x.ID).ToList();
             ViewBag.status_id = model.TRANGTHAIs.OrderByDescending(x => x.ID).ToList();
+
+
             return View();
 
         }
@@ -68,47 +63,63 @@ namespace TEAM13SEP.Areas.User.Controllers
 
         }
 
-
-
-        public ActionResult Like(int id)
+        [HttpGet]
+        public ActionResult Edit(int id)
         {
-            // cách này ổn, tuy nhiên vấn đề gặp phải là : Ai cũng like dc, ko coi được người like, like nhiều lần => đây là một tính năng :bonk:
 
-            GOPY update = model.GOPies.ToList().Find(u => u.ID == id);
-            update.nutLIKE += 1;
+
+
+
+            GOPY gopy = model.GOPies.Find(id);
+            if (gopy == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.chude_id = model.CHUDEs.OrderByDescending(x => x.ID).ToList();
+            ViewBag.admin_id = model.ADMINs.OrderByDescending(x => x.ID).ToList();
+            ViewBag.status_id = model.TRANGTHAIs.OrderByDescending(x => x.ID).ToList();
+            return View(gopy);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(
+                        GOPY gopy, int id)
+        {
+            if (ModelState.IsValid)
+            {
+                var gopy1 = model.GOPies.FirstOrDefault(x => x.ID == id);
+
+                gopy1.GOPY_TEN = gopy.GOPY_TEN;
+                gopy1.CHUDE_ID = gopy.CHUDE_ID;
+                gopy1.ADMIN_ID = gopy.ADMIN_ID;
+                gopy1.nutLIKE = gopy.nutLIKE;
+                gopy1.SINHVIEN_ID = gopy.SINHVIEN_ID;
+                gopy1.NOIDUNG_GOPY = gopy.NOIDUNG_GOPY;
+                gopy1.TRALOI_GOPY = gopy.TRALOI_GOPY;
+                gopy1.GOPY_STATUS = gopy.GOPY_STATUS;
+                gopy1.DATE = gopy.DATE;
+
+                model.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var gopy = model.GOPies.FirstOrDefault(x => x.ID == id);
+            return View(gopy);
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ComfirmDelete(int id)
+        {
+            var gopy = model.GOPies.FirstOrDefault(x => x.ID == id);
+            model.GOPies.Remove(gopy);
             model.SaveChanges();
             return RedirectToAction("Index");
-
-
-            // cách dưới làm sml vẫn ko ra
-
-
-            /* var postIDforLike = model.GOPies.Find(id);// add logic to get the id of the post to increment the likes
-             using (var _gopy = new SEPEntities())
-             {
-
-                 var addLikeSql = "update GOPY set nutLike = nutLike + 1 where ID = @id";
-                 var paramID = new SqlParameter("id", postIDforLike);
-                 _gopy.Database.ExecuteSqlCommand(addLikeSql, postIDforLike);
-             }
-             return RedirectToAction("Index");*/
         }
-        public ActionResult Like1(int id)
-        {
-            // cách này ổn, tuy nhiên vấn đề gặp phải là : Ai cũng like dc, ko coi được người like, like nhiều lần => đây là một tính năng :bonk:
-            // này là like khi đã dc trả lời
-
-            GOPY update = model.GOPies.ToList().Find(u => u.ID == id);
-            update.nutLIKE += 1;
-            model.SaveChanges();
-            return RedirectToAction("Index2");
-
-
-            
-        }
-
-
-        // dưới này là controller cho cách like mới
-
     }
 }

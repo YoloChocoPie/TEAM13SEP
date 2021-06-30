0998e2818163ee904b8ddecf3740115c05f5f766
187PM23334 - NGUYEN THANH HUNG - K24T-PM1 <hung.187pm23334@vanlanguni.vn>
2021-06-30 22:52:09 +0700

    Unit tesst cho chức năng  Đăng góp ý

---
+++ TEAM13SEP.Tests/Controllers/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using TEAM13SEP.Areas.User.Controllers;
+
+using TEAM13SEP.Models;
+using TEAM13SEP;
+
+namespace TEAM13SEP.Tests.Controllers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCreate()
+        {
+            var controller = new GopYController();
+
+            var result = controller.Create() as ViewResult;
+            Assert.IsNotNull(result);
+
+        }
+    }
+}

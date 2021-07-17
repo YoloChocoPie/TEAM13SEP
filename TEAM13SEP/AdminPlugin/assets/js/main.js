2bbe321d82dc5b7d5f02d5a7cbcc440d200bdf23
187PM14029 - Trinh Minh Trung - K24T-PM1 <trung.187pm14029@vanlanguni.vn>
2021-07-18 00:08:20 +0700

    giao diện người dùng v4

---
+++ TEAM13SEP/AdminPlugin/assets/js/main.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('.navbar').addClass('moving');
+            $('#logo').attr('src', 'img/logo-dark.png');
+        } else {
+            $('.navbar').removeClass('moving');
+            $('#logo').attr('src', 'img/logo-light.png');
+        }
+
+        if ($(this).scrollTop() > 250) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+
+    $('#back-to-top').click(function () {
+        return $('body, html').animate({ scrollTop: 0 }, 400), !1;
+    });
+});

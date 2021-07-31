2ca8175eb81aff8b069037ecfe98c31c310b4d37
YoloChocoPie <chocopieyolo@gmail.com>
2021-08-01 03:52:37 +0700

    help

--- TEAM13SEP/Models/Model1.Designer.cs
+++ TEAM13SEP/Models/Model1.Designer.cs
@@ -1,7 +1,4 @@
-
-// T4 code generation is enabled for model 'C:\Users\choco\Desktop\TEAM13\TEAM13SEP\Models\Model1.edmx'. 
-
-
+﻿// T4 code generation is enabled for model 'C:\Users\choco\Desktop\TEAM13\TEAM13SEP\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
